2aed0276493357cf3ed1416259584fd908e9732d
asuhhinin <asuhhinin@malwarebytes.com>
2021-03-02 10:51:50 +0200

    project structure refactor

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -1,17 +1,18 @@
 import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from '../plugins/unpkg-path-plugins';
-import { fetchPlugin } from '../plugins/fetch-plugin';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 let service: esbuild.Service;
 
-export default async (rawCode: string) => {
+const bundle = async (rawCode: string) => {
+
     if (!service) {
         service = await esbuild.startService({
             worker: true,
             wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
         });
     }
-    
+
     const result = await service.build( {
         entryPoints: ['index.js'],
         bundle: true,
@@ -27,3 +28,5 @@ export default async (rawCode: string) => {
     });
     return result.outputFiles[0].text
 };
+
+export default bundle;
