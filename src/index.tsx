d8a161f1cfc1161324819ef65812041c21ccf87a
asuhhinin <asuhhinin@malwarebytes.com>
2021-03-03 09:43:35 +0200

    wrap code editor and preview into cell  component

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,11 @@
 import ReactDOM from 'react-dom';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useState } from 'react';
-import CodeEditor from './components/code-editor'
-import Preview from './components/preview';
-import bundle from './bundler';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-    const [code, setCode] = useState('');
-    const [input, setInput] = useState('');
-
-    const onClick = async () => {
-        const output = await bundle(input);
-        setCode(output);
-    };
-
     return <div>
-        <CodeEditor 
-            initialValue=""
-            onChange={(value) => setInput(value)}
-        />
-        <div>
-            <button onClick={onClick}>Submit</button>
-        </div>
-        <Preview code={code} />
+        <CodeCell />
+        <CodeCell />
     </div>;
 }
 
