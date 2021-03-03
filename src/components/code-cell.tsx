c0ef2980d00b5365eac7eb0644264d781fae74b9
asuhhinin <asuhhinin@malwarebytes.com>
2021-03-03 09:55:07 +0200

    added resizable component and start wrap cell into it

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import CodeEditor from './code-editor'
 import Preview from './preview';
 import bundle from '../bundler';
+import Resizable from './resizable';
 
 const CodeCell = () => {
     const [code, setCode] = useState('');
@@ -12,16 +13,20 @@ const CodeCell = () => {
         setCode(output);
     };
 
-    return <div>
-        <CodeEditor
-            initialValue=""
-            onChange={(value) => setInput(value)}
-        />
-        <div>
-            <button onClick={onClick}>Submit</button>
-        </div>
-        <Preview code={code} />
-    </div>;
+    return (
+        <Resizable direction='vertical'>
+            <div>
+                <CodeEditor
+                    initialValue=""
+                    onChange={(value) => setInput(value)}
+                />
+                <div>
+                    <button onClick={onClick}>Submit</button>
+                </div>
+                <Preview code={code} />
+            </div>
+        </Resizable>
+    );
 }
 
 export default CodeCell;
