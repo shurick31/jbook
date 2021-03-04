9fe062f34bd76ef0e37be3231554754e660852c5
asuhhinin <asuhhinin@malwarebytes.com>
2021-03-04 09:19:34 +0200

    added delays in input and  code execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CodeEditor from './code-editor'
 import Preview from './preview';
 import bundle from '../bundler';
@@ -8,10 +8,17 @@ const CodeCell = () => {
     const [code, setCode] = useState('');
     const [input, setInput] = useState('');
 
-    const onClick = async () => {
-        const output = await bundle(input);
-        setCode(output);
-    };
+    useEffect(()=>{
+        let timer: any;
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(async () => {
+            const output = await bundle(input);
+            setCode(output);
+        }, 800);
+        return () => {clearTimeout(timer);}
+    }, [input])
 
     return (
         <Resizable direction='vertical'>
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -32,7 +32,10 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
     useEffect(() => {
         iframe.current.srcdoc = html;
-        iframe.current.contentWindow.postMessage(code, '*');
+        const timer = setTimeout(() => {
+            iframe.current.contentWindow.postMessage(code, '*');
+        }, 50);
+        return () => { clearTimeout(timer)}
     }, [code]);
 
     return <div className="preview-wrapper">
