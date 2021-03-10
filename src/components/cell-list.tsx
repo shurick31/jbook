24595f15deb67b09b5ed7ff1f2dc0a3c34fe2393
asuhhinin <asuhhinin@malwarebytes.com>
2021-03-10 11:20:43 +0200

    fix child key error

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import AddCell from './add-cell';
 import CellListItem from './cell-list-item';
@@ -5,10 +6,10 @@ import CellListItem from './cell-list-item';
 const CellList: React.FC = () => {
     const cells = useTypedSelector(({ cells: { order, data }}) => order.map(id =>  data[id]));
 
-    const renderedCells = cells.map(cell => <>
+    const renderedCells = cells.map(cell => <Fragment key={cell.id}>
         <AddCell nextCellId={cell.id}/>
-        <CellListItem key={cell.id} cell={cell} />
-        </>)
+        <CellListItem  cell={cell} />
+        </Fragment>)
 
     return <div>
         {renderedCells}
