144ff42c5a50f0da311bdee96b35fd6ee6e9360e
asuhhinin <asuhhinin@malwarebytes.com>
2021-03-10 11:02:29 +0200

    action bar created and styled

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -1,5 +1,6 @@
 import { useActions } from '../hooks/use-actions';
 import ActionButton from './action-button';
+import './action-bar.css';
 
 interface ActionBarProps {
     id: string;
@@ -7,7 +8,7 @@ interface ActionBarProps {
 
 const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
     const { moveCell, deleteCell } = useActions();
-    return (<div>
+    return (<div className='action-bar'>
         <ActionButton
             onClick={() => moveCell(id, 'up')}
             className='fa-arrow-up' />
