9e40dec5409c1abd6e4ad16bec735b4e839a74eb
asuhhinin <asuhhinin@malwarebytes.com>
2021-03-10 11:19:00 +0200

    add AddCell component into cell listt

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,12 +1,18 @@
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import AddCell from './add-cell';
 import CellListItem from './cell-list-item';
 
 const CellList: React.FC = () => {
     const cells = useTypedSelector(({ cells: { order, data }}) => order.map(id =>  data[id]));
 
-    const renderedCells = cells.map(cell => <CellListItem key={cell.id} cell={cell} />)
+    const renderedCells = cells.map(cell => <>
+        <AddCell nextCellId={cell.id}/>
+        <CellListItem key={cell.id} cell={cell} />
+        </>)
+
     return <div>
         {renderedCells}
+        <AddCell nextCellId={null}/>
     </div>
 }
 
