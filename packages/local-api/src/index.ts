f06bbf5c5892fec11fe8661e4e94a0bcc29e49bc
asuhhinin <asuhhinin@malwarebytes.com>
2021-03-22 10:16:40 +0200

    Added fetch cells api and actions

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,22 +1,25 @@
 import express from 'express'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 import path from 'path'
+import { createCellsRouter } from './routes/cell'
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (port: number, filename: string, dir: string, useProxy: boolean) => {
     const app = express()
 
-    const packagePath = require.resolve('local-client/build/index.html')
-    app.use(express.static(path.dirname(packagePath)))
+    if (useProxy) {
+        app.use(createProxyMiddleware({
+            target: 'http://localhost:3000',
+            ws: true,
+            logLevel: 'silent',
+        }))
+    } else {
+        const packagePath = require.resolve('local-client/build/index.html')
+        app.use(express.static(path.dirname(packagePath)))
+    }
 
-    
-    // app.use(createProxyMiddleware({
-    //     target: 'http://localhost:3000',
-    //     ws: true,
-    //     logLevel: 'silent',
-    // }))
+    app.use(createCellsRouter(filename, dir))
 
     return new Promise<void>((resolve, reject) => {
         app.listen(port, resolve).on('error', reject)
     })
-
 }
