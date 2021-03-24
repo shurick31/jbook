7c60882073db07fd36298dfb979a3e3eb968d701
asuhhinin <asuhhinin@malwarebytes.com>
2021-03-24 10:04:59 +0200

    persist middleware added to cave cells

--- packages/local-client/src/state/store.ts
+++ packages/local-client/src/state/store.ts
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducers';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
+import { persistMiddleware } from './middlewares/persist-middleware'
 
 import { ActionType } from './action-types';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+export const store = createStore(reducers, {}, applyMiddleware(persistMiddleware,thunk));
 
 
